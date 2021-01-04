931be1e35f5fd0de7792b2fa2ed88636c97cf898
KOIZUMI teruaki <1755900+terukizm@users.noreply.github.com>
2021-01-05 04:27:59 +0900

    refactor: ジャンルの定義情報を定数ファイルに移動

--- src/App.vue
+++ src/App.vue
@@ -192,6 +192,8 @@
 </style>
 
 <script>
+import constant from './constant';
+
 export default {
   data() {
     return {
@@ -201,58 +203,7 @@ export default {
   },
   computed: {
     genres: () => {
-      return {
-        1: {
-          name: "居酒屋・バー・バル",
-          color: "yellow-500",
-          icon: require("@/assets/img/marker/genre1.png")
-        },
-        2: {
-          name: "和食・寿司",
-          color: "green-500",
-          icon: require("@/assets/img/marker/genre2.png")
-        },
-        3: {
-          name: "洋食",
-          color: "blue-500",
-          icon: require("@/assets/img/marker/genre3.png")
-        },
-        4: {
-          name: "中華",
-          color: "red-500",
-          icon: require("@/assets/img/marker/genre4.png")
-        },
-        5: {
-          name: "麺類(ラーメン、そば、うどん)",
-          color: "yellow-300",
-          icon: require("@/assets/img/marker/genre5.png")
-        },
-        6: {
-          name: "カレー・各国料理・創作料理",
-          color: "purple-400",
-          icon: require("@/assets/img/marker/genre6.png")
-        },
-        7: {
-          name: "ステーキ・鉄板焼き・焼肉",
-          color: "yellow-600",
-          icon: require("@/assets/img/marker/genre7.png")
-        },
-        8: {
-          name: "ファーストフード・ファミレス・食堂",
-          color: "blue-300",
-          icon: require("@/assets/img/marker/genre8.png")
-        },
-        9: {
-          name: "カフェ・スイーツ",
-          color: "pink-400",
-          icon: require("@/assets/img/marker/genre9.png")
-        },
-        10: {
-          name: "その他",
-          color: "gray-400",
-          icon: require("@/assets/img/marker/genre10.png")
-        }
-      };
+      return constant.GENRES;
     }
   }
 };
