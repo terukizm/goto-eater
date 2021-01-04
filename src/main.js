dd5856de17b14b77fa61c453558a5968a26e5515
KOIZUMI teruaki <1755900+terukizm@users.noreply.github.com>
2021-01-05 02:21:42 +0900

    feat: alpine.jsのx-dataとx-showをそれぞれdata(), v-showに置換して動作確認

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from "vue";
-import App from "./Sample.vue";
+import App from "./App.vue";
+// import App from "./Sample.vue";
 import router from "./router";
+
+import './tailwindcss.css';
 
 Vue.config.productionTip = false;
 
