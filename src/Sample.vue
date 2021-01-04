58867579dc0283da224dc3258be873249d15d310
KOIZUMI teruaki <1755900+terukizm@users.noreply.github.com>
2021-01-04 22:55:34 +0900

    feat: 自動生成されたApp.vueをSample.vueにリネーム

--- src/Sample.vue
+++ src/Sample.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <p> this is vue App.vue (sample) </p>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
