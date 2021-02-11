0767b0c000b90a01e3dd73bcb14a8b2e5c004d55
KOIZUMI teruaki <1755900+terukizm@users.noreply.github.com>
2021-02-11 18:24:22 +0900

    doc: 利用規約のDAMS関係の文言を修正

--- src/components/Dialog.vue
+++ src/components/Dialog.vue
@@ -20,8 +20,8 @@ export default {
         const html = `
         <div class="text-sm">
         <p class="p-2">・各都道府県のGoToEat公式サイトの情報を元に地図にプロットしていますが、内容の即時性、および正当性を担保するものではありません。</p>
-        <p class="p-2">・特にお店の場所は<a href="https://newspat.csis.u-tokyo.ac.jp/geocode/modules/dams/index.php?content_id=1" class="underline">使用しているジオコーダ(DAMS)</a>の都合上、おおよその位置しか表示できません。
-          </strong><strong>必ずGoogleMapなど、他サービスも合わせてご確認ください。</strong></p>
+        <p class="p-2">・特にお店の場所は<a href="https://newspat.csis.u-tokyo.ac.jp/geocode/modules/dams/index.php?content_id=1" target="_blank" class="underline">使用しているライブラリ</a>の都合上、おおよその位置しか表示できません。
+          </strong><strong>必ずGoogleMapなど、他サービスの結果と合わせてご確認ください。</strong></p>
         <p class="p-2">・現在地取得のために端末の位置情報を取得しますが、<strong>利用者個人を特定できるような情報は保持していません。</strong><br>
           ただし、アクセス解析目的で<a href="https://policies.google.com/technologies/partner-sites?hl=ja" target="_blank" class="underline">Google Analytics</a>を有効化しています。<strong>気になる方はCookieをオプトアウト</strong>してご利用ください。</p>
         <p class="p-2 mt-2"><b>本サービスを用いたことで発生した問題に関して、開発者は一切の責任を負いません。</b></p>
